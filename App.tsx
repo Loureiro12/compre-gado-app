4285fc18f0c934415f6529cbcaba1ce8d08ae052
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2023-07-05 16:55:06 -0300

    feat: adding images from APP

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import "react-native-gesture-handler";
+import codePush from "react-native-code-push";
 import { ThemeProvider } from "styled-components/native";
 import FlashMessage from "react-native-flash-message";
 // import { AppProvider } from "./src/hooks";
@@ -14,8 +15,32 @@ import theme from "./src/theme";
 import { Loading } from "./src/components/Loading";
 import { Routes } from "./src/routes/index";
 import { AppProvider } from "./src/hooks";
+import { Platform } from "react-native";
 
-export default function App() {
+const CODEPUSH_KEY =
+  Platform.OS === "android"
+    ? "Qy8Jsz6tup7an_r99n0PS_2vJQ4465SCOkqu3"
+    : "ax4dfxDLfU_2vWCW-6ZTc2ZZy4ixvtSvbzBX-";
+
+const codePushOptions = {
+  checkFrequency: codePush.CheckFrequency.ON_APP_START,
+  mandatoryInstallMode: codePush.InstallMode.IMMEDIATE,
+  deploymentKey: CODEPUSH_KEY,
+  updateDialog: {
+    appendReleaseDescription: false,
+    title: "Atualização disponível",
+    mandatoryUpdateMessage:
+      "Uma nova atualização está disponível para ser instalada.",
+    mandatoryContinueButtonLabel: "Instalar",
+    optionalUpdateMessage:
+      "Uma nova atualização está disponível para ser instalada.",
+    optionalInstallButtonLabel: "Instalar",
+    optionalIgnoreButtonLabel: "Ignorar",
+  },
+  // installMode: codePush.InstallMode.IMMEDIATE,
+};
+
+const App = () => {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
@@ -24,4 +49,6 @@ export default function App() {
       <FlashMessage position="top" />
     </ThemeProvider>
   );
-}
+};
+
+export default codePush(codePushOptions)(App);
